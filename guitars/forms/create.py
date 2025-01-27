107eb0c8f1e6a8261a3caf890454cb3ef303d299
Victor <vityasafronuk30@gmail.com>
2025-01-27 17:03:15 +0200

    almolst

--- guitars/forms/create.py
+++ guitars/forms/create.py
@@ -4,4 +4,4 @@ from guitars.models import Guitar
 class GuitarCreateForm(forms.ModelForm):
     class Meta:
         model = Guitar
-        fields = ['brand', 'model', 'color', 'type', 'year', 'price']
+        fields = ['brand', 'model', 'color', 'type', 'year', 'price', 'avatar', 'description']
--- guitars/models.py
+++ guitars/models.py
@@ -1,13 +1,18 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 class Guitar(models.Model):
     brand = models.CharField(max_length=100)
     model = models.CharField(max_length=100)
-    color = models.CharField(max_length=50)
+    color = models.CharField(max_length=50, blank=True, null=True, default=None)
     type = models.CharField(max_length=50)
     year = models.IntegerField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    avatar = models.ImageField(upload_to='guitars/avatars/')
-
+    avatar = models.ImageField(upload_to='guitars/avatars/', blank=True, null=True, default=None)
+    description = models.TextField(max_length=500, blank=True, null=True, default=None)
+    def clean(self):
+        if self.year < 1900 or self.year > 2100:
+            raise ValidationError('Year must be between 1900 and 2100.')
+        if self.price < 0:
+            raise ValidationError('Price must be a positive number.')
     def __str__(self):
         return f"{self.brand} {self.model} ({self.year})"
-# Create your models here.
